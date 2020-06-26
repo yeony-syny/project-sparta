5a4179c64f12e080580ab56689a266972127ffcf
sooyeon <synoout@gmail.com>
2020-06-27 01:58:28 +0900

    20200636 서버 연결 안됨

--- app.py
+++ app.py
@@ -8,21 +8,13 @@ db = client.dbnetflixTV
 ## URL 별로 함수명이 같거나,
 ## route('/') 등의 주소가 같으면 안됩니다.
 
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 #  API역할을 하는 부분
-
-# @app.route('/member/netfilx', methods=['GET']) - 삭제
-
-# 검색어를 주고
-@app.route('/search', methods=['GET'])
-def search_list():
-    tvpros = list(db.tvprogram.find({},{'_id':False}))
-
-    return jsonify({'result': 'success','search_list':tvprosss})
-
 
 
 # 검색어와 일치하는 콘텐츠를 찾아 돌려준다
@@ -30,10 +22,28 @@ def search_list():
 def search_post():
 
     title_receive = request.form['title_give']
-    tvpro = db.tvprogram.find_one({'title':title_receive})
-    
 
-    return jsonify({'result': 'success','msg':'검색어찾기 연결'})
+    program = db.tvprogram.find({'title':title_receive})
+
+    # for pro in program :
+    # print(pro)    
+
+    return jsonify({'result': 'success','msg':'검색결과 보내기 연결'})
+
+    # 1. 클라이언트가 준 title 가져오기
+    # 2. DB에서 정보 찾기
+    # 3. 성공 여부 확인
+    # 4. 성공 메세지(=검색결과) 반환하기
+
+
+
+
+    # 검색어를 주고
+@app.route('/search', methods=['GET'])
+def search_list():
+    result = list(db.tvprogram.find({},{'_id':0}))
+
+    return jsonify({'result': 'success','search_results':result})
 
 
 
