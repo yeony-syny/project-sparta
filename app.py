b9ca95ef4f1723bdd6dff5a7246313fd400fd319
sooyeon <synoout@gmail.com>
2020-06-28 02:22:19 +0900

    20200628 02:20 서버-클라이언트 연결/검색결과만 출력하기

--- app.py
+++ app.py
@@ -15,22 +15,29 @@ def home():
 
 @app.route('/search', methods=['POST'])
 def search_post():
+    # 1. 클라이언트가 준 title 가져오기
+    # 2. DB에서 정보 찾기
+    # a = list(db.tvprogram.find({},{'_id':0})
+    # 3. 성공 여부 확인
+    # 4. 성공 메세지(=검색결과) 반환하기
 
     title_receive = request.form['title_give']
-    ssresult = list(db.tvprogram.find({'title':title_receive}))
+    # ssresult = list(db.tvprogram.find({'title':title_receive}))
 
-    print(ssresult)
+    # print(ssresult)
 
-    return jsonify({'result': 'success','msg':'검색결과 보내기 연결'})
+    return jsonify({'result': 'success','correct_title':title_receive})
 
 
 
 @app.route('/search', methods=['GET'])
 def search_list():
+    # 1. 모든 /search의 문서를 가져온 후 list로 변환하기
+    # 2. 성공메세지와 함께 리스트 보내기
 
     sssresult = list(db.tvprogram.find({},{'_id':0}))
-
-    return jsonify({'result': 'success','search_results':sssresult})
+    
+    return jsonify({'result': 'success','sssresult':sssresult})
 
 
 
@@ -39,8 +46,3 @@ if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
     
-    # 1. 클라이언트가 준 title 가져오기
-    # 2. DB에서 정보 찾기
-    # a = list(db.tvprogram.find({},{'_id':0})
-    # 3. 성공 여부 확인
-    # 4. 성공 메세지(=검색결과) 반환하기
